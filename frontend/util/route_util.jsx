85787bd3205fa5a5cee7d100785af032bdeee06f
JortzFromSchool <johnnydortiz@gmail.com>
2021-11-02 16:32:58 -0400

    auth working, upload to heroku

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -2,23 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
+//compiled from App Academy BenchBnB tutorial
+
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
-    !loggedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/" />
-    )
+    !loggedIn ? (<Component {...props} />) : ( <Redirect to="/" />)
   )} />
 );
 
 const Protected = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
-     loggedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/login" />
-    )
+     loggedIn ? ( <Component {...props} /> ) : ( <Redirect to="/login" />)
   )} />
 );
 
